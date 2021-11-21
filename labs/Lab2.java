2532c2c2d2426f3a0be60270189b24203f75fbdc
Cheems <stepan.privalov04@gmail.com>
2021-11-21 19:31:14 +0300

    Some changes to Lab2

--- labs/Lab2.java
+++ labs/Lab2.java
@@ -1,4 +1,3 @@
-package com.shkilo;
 import java.util.Scanner;
 
 public class Lab2 {
@@ -9,22 +8,27 @@ public class Lab2 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        printText("Enter the number");
+        printText("Enter the number less than 15");
         int n = in.nextInt();
-        int fact = 1;
-        for (int i = 2;i <= n;i++)
+        if (n<15&&n>0) {
+            int fact = 1;
+            for (int i = 2; i <= n; i++) {
+                fact = fact * i;
+            }
+
+            int fact2 = 1;
+            int i = 2;
+            while (i <= n) {
+                fact2 = fact2 * i;
+                i++;
+            }
+
+            System.out.printf("The factorial is - %d(for), %d(while)", fact, fact2);
+
+        }
+        else
         {
-            fact = fact * i;
+            System.out.println("Wrong input data!");
         }
-
-        int fact2 = 1;
-        int i = 2;
-        while (i <= n) {
-            fact2 = fact2 * i;
-            i++;
-        }
-
-        System.out.printf("The factorial is - %d(for), %d(while)", fact, fact2);
-
     }
 }
