85d370de61e44af5a63ba7b784ef1f01765005d0
Cheems <stepan.privalov04@gmail.com>
2022-03-04 09:46:21 +0300

    Few mistakes cleared

--- labs/CalcFractions/Lab8.java
+++ labs/CalcFractions/Lab8.java
@@ -2,7 +2,7 @@ package calculator
 import java.util.Scanner;
 import static calculator.Calc.*;
 
-public class Source 
+public class Lab8 
 {
     public static void main(String[] args) throws Exception 
     {
